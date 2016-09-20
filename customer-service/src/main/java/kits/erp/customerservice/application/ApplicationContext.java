c243534cbee01aa5af1e8bcb4588ba05602351b4
Sandor Gabor Kocso <kocso.sandor.gabor@gmail.com>
2016-09-20 19:28:43 +0200

    ApplicationContext injection

--- customer-service/src/main/java/kits/erp/customerservice/application/ApplicationContext.java
+++ customer-service/src/main/java/kits/erp/customerservice/application/ApplicationContext.java
@@ -5,28 +5,10 @@ import kits.erp.customerservice.domain.core.CustomerService;
 
 public class ApplicationContext {
 
-	private static ApplicationContext instance;
-	
 	public final CustomerService customerService;
 
-	private ApplicationContext(CustomerService customerService) {
-		this.customerService = customerService;
-	}
-	
-	public static void init(CustomerRepository customerRepository){
-		if(instance == null) {
-			instance = new ApplicationContext(new CustomerService(customerRepository));
-		} else {
-			throw new IllegalStateException("ApplicationContext is already initialized");
-		}
-	}
-	
-	public static ApplicationContext get(){
-		if(instance == null) {
-			throw new IllegalStateException("ApplicationContext is not yet initialized");
-		} else {
-			return instance;
-		}
+	public ApplicationContext(CustomerRepository customerRepository) {
+		this.customerService = new CustomerService(customerRepository);
 	}
 	
 }
